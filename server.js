3824472b69d116dbe892123aab230d640b442272
Elvis Knapman <elvis@elvisknapman.com>
2019-09-04 15:33:31 -0400

    :tada: Initial commit

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+const userRouter = require("./users/userRouter.js");
+
+server.use(express.json());
+
+server.use("/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,38 @@
-const express = 'express';
+const express = require("express");
+const userDB = require("./userDb.js");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
+router.post("/:id/posts", (req, res) => {});
+
+router.get("/", (req, res) => {
+  userDB
+    .get()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "could not treieve users from the database" });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
