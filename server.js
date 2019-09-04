baf0f51a33d00586d52051010e113ab37c46cb53
Elvis Knapman <elvis@elvisknapman.com>
2019-09-04 17:49:12 -0400

    :sparkles: imported post router and added it as middleware

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const server = express();
 
 const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter.js");
 
 server.use(express.json());
 server.use(logger);
 
 server.use("/users", userRouter);
+server.use("/posts", postRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
