3484b29edf8f42a327ee494af6a1e5683b0f5cf1
Elvis Knapman <elvis@elvisknapman.com>
2019-09-04 16:24:16 -0400

    :sparkles: wrote middleware and request handlers
    wrote validatePost and validateUserID middlewares, /:id and :/id/posts  GET handlers

--- users/userRouter.js
+++ users/userRouter.js
@@ -19,9 +19,25 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", validateUserId, (req, res) => {
+  res.status(200).json(req.user);
+});
 
-router.get("/:id/posts", (req, res) => {});
+router.get("/:id/posts", validateUserId, (req, res) => {
+  const { id } = req.params;
+  userDB
+    .getUserPosts(id)
+    .then(posts => {
+      console.log(posts);
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "Could not retrieve the posts for the user" });
+    });
+});
 
 router.delete("/:id", (req, res) => {});
 
@@ -29,10 +45,40 @@ router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+  const { id } = req.params;
+
+  userDB
+    .getById(id)
+    .then(user => {
+      console.log("user in validate", user);
+      if (user) {
+        req.user = user;
+        console.log("user in req.user", req.user);
+        next();
+      } else {
+        return res.status(400).json({ message: "Invalid user ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
 function validateUser(req, res, next) {}
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  const { text } = req.body;
+
+  if (!req.body) {
+    return res.status(400).json({ message: "missing post data" });
+  }
+
+  if (!text) {
+    return res.status(400).json({ message: "missing required text field" });
+  }
+
+  next();
+}
 
 module.exports = router;
