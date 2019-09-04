a0da64789ce498a2652ccd782f6ecf09001290fa
Elvis Knapman <elvis@elvisknapman.com>
2019-09-04 17:59:09 -0400

    :sparkles: wrote DELETE handler for deleting a post

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -19,7 +19,24 @@ router.get("/:id", validatePostId, (req, res) => {
   res.status(200).json(req.post);
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", validatePostId, (req, res) => {
+  const { id } = req.params;
+
+  postDB
+    .remove(id)
+    .then(result => {
+      // result contains number of records deleted
+      if (result) {
+        res.status(200).json({ message: "post successfully deleted" });
+      } else {
+        res.status(400).json({ message: "no posts were deleted" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "post could not be deleted" });
+    });
+});
 
 router.put("/:id", (req, res) => {});
 
